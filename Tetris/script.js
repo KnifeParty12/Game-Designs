9379f238e4fdf617fa57c04a1be82e343d6a9193
KnifeParty12 <flashfunk01@gmail.com>
2019-03-05 03:32:53 +0530

    Added Color Mapping To Various Pieces

--- Tetris/script.js
+++ Tetris/script.js
@@ -4,11 +4,6 @@ const context = canvas.getContext('2d');
 context.scale(20,20);
 
 
-const matrix = [
-    [0,0,0],
-    [1,1,1],
-    [0,1,0]
-];
 
 function collide(arena,player) {
     const [m, o] = [player.matrix, player.pos];
@@ -42,48 +37,48 @@ function createPiece(type) {  //https:// Source For Pieces:: medium.com/@markmli
         ];
     } else if (type === 'O'){
         return [
-            [1,1],
-            [1,1]
+            [2,2],
+            [2,2]
         ];
     }
     else if (type === 'L'){
         return [
-            [0,1,0],
-            [0,1,1],
-            [0,1,1],
+            [0,3,0],
+            [0,3,1],
+            [0,3,1],
 
         ];
     }
     else if (type === 'J'){
         return [
-            [0,1,0],
-            [0,1,0],
-            [1,1,0],
+            [0,4,0],
+            [0,4,0],
+            [4,4,0],
 
         ];
     }
     else if (type === 'I'){
         return [
-            [0,1,0,0],
-            [0,1,0,0],
-            [0,1,0,0],
-            [0,1,0,0]
+            [0,5,0,0],
+            [0,5,0,0],
+            [0,5,0,0],
+            [0,5,0,0]
 
         ];
     }
 
     else if (type === 'S'){
         return [
-            [0,1,1],
-            [1,1,0],
+            [0,6,6],
+            [6,6,0],
             [0,0,0],
 
         ];
     }
     else if (type === 'Z'){
         return [
-            [1,1,1],
-            [0,1,1],
+            [7,7,7],
+            [0,7,7],
             [0,0,0],
 
         ];
@@ -104,7 +99,7 @@ function drawMatrix(matrix,offset){
     matrix.forEach((row, y) => {
     row.forEach((value, x) => {
         if(value!==0) {
-            context.fillStyle = 'red';
+            context.fillStyle = colors[value];
             context.fillRect(x + offset.x,
                                 y + offset.y,
                             1, 1);
@@ -203,6 +198,18 @@ function update(time = 0) {
     draw();
     requestAnimationFrame(update)
 }
+
+const colors = [
+    null,
+    'red',
+    'blue',
+    'violet',
+    'green',
+    'yellow',
+    'orange',
+    'pink',
+];
+
 
 const arena = createMatrix(12, 20);
 
