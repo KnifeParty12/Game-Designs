807010d8e298d30d58b490743097c1ee5ad46ac3
KnifeParty12 <flashfunk01@gmail.com>
2019-03-05 02:45:38 +0530

    Added Rotate Matrix Functionality to Rotate given piece

--- Tetris/script.js
+++ Tetris/script.js
@@ -82,10 +82,30 @@ function playerMove(dir){
     }
 }
 
+function rotate(matrix, dir){
+    for (let y =0; y < matrix.length; y++){
+        for (let x =0; x < y; x++){
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ];
+        }
+    }
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else{
+        matrix.reverse();
+    }
+}
+
 let dropCounter = 0;
 let dropInterval = 1000; //ms
 
 let lastTime =0;
+
 function update(time = 0) {
     const deltatime = time - lastTime;
     lastTime = time;
