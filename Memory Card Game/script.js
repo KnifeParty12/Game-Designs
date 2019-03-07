f9fac124e90a8c69845f4607bd7348013ae75477
KnifeParty12 <flashfunk01@gmail.com>
2019-03-07 19:49:10 +0530

    Design of Gameboard and onclick flip

--- Memory Card Game/script.js
+++ Memory Card Game/script.js
@@ -1,1 +1,10 @@
-  
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipcard(){
+    this.classList.toogle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipcard));
